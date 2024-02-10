f401df8644bdec7ee5b7aee995439611d63fae0d
UtkuMert <65029733+UtkuMert@users.noreply.github.com>
2024-02-10 23:25:18 +0300

    Game and Main class added

---
+++ src/Game.java
@@ -0,0 +1,9 @@
+public class Game {
+
+    public void start(){
+        System.out.println("Time to start!");
+        String nickName = "uroborus";
+        System.out.println("Welcome " + nickName);
+        System.out.println("Lets show us, you are a warrior or not.");
+    }
+}
